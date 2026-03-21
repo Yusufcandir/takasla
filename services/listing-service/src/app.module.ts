a9743069e510c3dd03e5db7bb3e0bfc0b6872876
Yusuf Candır <yusufcandir30@gmail.com>
2026-03-21 15:23:02 +0300

    Register ListingReportEntity in DatabaseModule entities

--- services/listing-service/src/app.module.ts
+++ services/listing-service/src/app.module.ts
@@ -8,13 +8,14 @@ import { ListingEntity } from './listings/listing.entity';
 import { ListingImageEntity } from './listings/listing-image.entity';
 import { ListingQuestionEntity } from './listings/listing-question.entity';
 import { ListingFavoriteEntity } from './listings/listing-favorite.entity';
+import { ListingReportEntity } from './listings/listing-report.entity';
 import { CategoryEntity } from './categories/category.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule.forRoot({
-      entities: [ListingEntity, ListingImageEntity, ListingQuestionEntity, ListingFavoriteEntity, CategoryEntity],
+      entities: [ListingEntity, ListingImageEntity, ListingQuestionEntity, ListingFavoriteEntity, ListingReportEntity, CategoryEntity],
       migrationsDir: join(__dirname, 'migrations'),
       dbHostEnv: 'LISTING_DB_HOST',
       dbPortEnv: 'LISTING_DB_PORT',
